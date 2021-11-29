eb72db9fbc17769defa05728c306ec9bc22b8cd4
JiahaoHong1997 <hongjiahao1997@gmail.com>
2021-11-29 23:11:01 +0800

    http-base

--- example/http-base/main.go
+++ example/http-base/main.go
@@ -9,6 +9,13 @@ import (
 func main() {
 	r := altria_web.Saber()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf()
+		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 	})
+	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+
+	r.Run(":9999")
 }

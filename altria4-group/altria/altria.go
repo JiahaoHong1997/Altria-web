3f118b34850ad17795ebd3ae337af98ffee72748
JiahaoHong1997 <hongjiahao1997@gmail.com>
2021-12-01 22:59:22 +0800

    group_control

---
+++ altria4-group/altria/altria.go
@@ -0,0 +1,67 @@
+package altria
+
+import (
+	"log"
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by altria
+type HandlerFunc func(*Context)
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
+
+type Engine struct {
+	*RouterGroup
+	router *router
+	groups []*RouterGroup // store all groups
+}
+
+// Saber() is the construction of altria.Engine
+func Saber() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+// Group is defined to create new RouterGroup
+// remember all group share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, hangdler HandlerFunc) {
+	group.addRoute("POST", pattern, hangdler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(port string) (err error) {
+	return http.ListenAndServe(port, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c := newContext(w, r)
+	engine.router.handle(c)
+}
---
+++ altria4-group/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"altria"
+	"net/http"
+)
+
+func main() {
+	r := altria.Saber()
+	r.GET("/index", func(c *altria.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *altria.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(c *altria.Context) {
+			// expect /hello?name=jiahaohong
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *altria.Context) {
+			// expect /hello/jiahaohong
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *altria.Context) {
+			c.JSON(http.StatusOK, altria.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
+
+	r.Run(":9999")
+}

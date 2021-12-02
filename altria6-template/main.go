65d776974bdbe35e05d17f8d9a46d7922a6a2885
JiahaoHong1997 <hongjiahao1997@gmail.com>
2021-12-02 21:48:16 +0800

    group_template

---
+++ altria6-template/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"altria"
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
+func main() {
+	r := altria.Saber()
+	r.Use(altria.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+
+	stu1 := &student{Name: "Geektutu", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
+	r.GET("/", func(c *altria.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *altria.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", altria.H{
+			"title":  "gee",
+			"stuArr": [2]*student{stu1, stu2},
+		})
+	})
+
+	r.GET("/date", func(c *altria.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", altria.H{
+			"title": "gee",
+			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
+		})
+	})
+
+	r.Run(":9999")
+}
